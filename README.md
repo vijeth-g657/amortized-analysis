7b750cf9226399afcd916cf31c077788ca1a5df5
VikasG-1 <vikivikasg@gmail.com>
2024-07-14 18:11:48 +0530

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+
+# Quiz-Application
+Quiz-Application Using MERN Stack Technology: In this project there are two portals one for admin and one for user <br><b>Admin:</b> admin can create quiz , add question to the quiz , modify the quiz , modify the answers or questions and can view reports of all users <br><b>User:</b> user can login and attempt the quiz and view the result and find the answers of all the question  quiz completion
+##How to run this 
+#
+## Installation
+
+```bash
+# Make sure you have your own url for mongodb cluster add it in the .env file to store data in your databases for Ex-
+mongodb+srv://<Name>:<Password>@cluster0.4u4rtje.mongodb.net/
+
+# Install dependencies for server in the root directory
+npm install
+# and run the server
+npm start
+
+# open the another terminal and go to the client directory and install the dependencies for client
+cd .\client\
+npm install
+# after installation run the client
+npm start
+
+
+# Server runs on  http://localhost:3000
+```
+
+
+
+<i><h2>1) Registration Page</h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/register.png"/>
+<i><h2>2) Login Page</h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/login.png"/>
+<i><h2>3) Admin Profile Home Page</h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/admin_homepage.png"/>
+<i><h2>4) Add Quiz Page in which Admin Post the Quiz into the MongoDB Database</h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/admin_add_exam.png"/>
+<i><h2>5) Add Questions in Quiz </h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/admin_add_ques.png"/>
+<i><h2>6) Edit Exam by Admin </h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/admin_edit_exam.png"/>
+<i><h2>7) Edit Que by Admin </h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/admin_edit_que.png"/>
+<i><h2>8) User's Profile </h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/user_home.png"/>
+<i><h2>9) Instructions before exam </h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/User_instruction.png"/>
+
+<i><h2>10) User Result</h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/user_result.png"/>
+
+<i><h2>11) Reports of all users that can be seen by only admin </h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/admin_report.png"/>
+<i><h2>12) User can see only his/her report </h2></i>
+<img src="https://github.com/kanishkpatel1/Quiz-Application/blob/main/images/user_report.png"/>
---
+++ client/src/pages/common/Visualize/Visualize.js
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from 'react';
+import './Visualize.css';
+// import amor1Image from '../images/amor1.jpg'; 
+
+
+
+const Visualize = () => {
+  const [stack, setStack] = useState([]);
+  const [totalAmortizedCost, setTotalAmortizedCost] = useState(0);
+  const [inputVisible, setInputVisible] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [amortizedCost, setAmortizedCost] = useState(0);
+  const [xValue, setXValue] = useState(0);
+  const [yValue, setYValue] = useState(0);
+  const [resultValue, setResultValue] = useState(0);
+  const [beforeValue, setBeforeValue] = useState(0);
+  const [afterValue, setAfterValue] = useState(0);
+  const [animationClass, setAnimationClass] = useState('');
+  const [temp, setTemp] = useState('');
+
+
+  useEffect(() => {
+    const handleDOMContentLoaded = () => {
+      // Any additional setup can go here
+    };
+    document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
+    return () => {
+      document.removeEventListener('DOMContentLoaded', handleDOMContentLoaded);
+    };
+  }, []);
+  
+  
+
+  const toggleInputField = () => {
+    setInputVisible(!inputVisible);
+  };
+
+  const updateTotalAmortizedCost = (cost) => {
+    setTotalAmortizedCost((prevCost) => prevCost + cost);
+  };
+
+  const push = () => {
+    const newStack = [...stack];
+    newStack.unshift(inputValue);
+
+    const x = newStack.length - 1;
+    const y = x + 1;
+    const amortizedCost = calculatePushAmortizedCost(x, y);
+
+    setBeforeValue(x);
+    setAfterValue(y);
+    setAnimationClass('show-animation');
+    setTemp('temp');
+    setXValue(x);
+    setYValue(y);
+    
+    setTimeout(() => {
+      setStack(newStack);
+      setAmortizedCost(amortizedCost);
+      updateTotalAmortizedCost(amortizedCost);
+    //   setXValue(x);
+    //   setYValue(y);
+      setResultValue(amortizedCost);
+      setInputValue('');
+      setInputVisible(false);
+      setAnimationClass('');
+      setTemp('');
+    }, 3000);
+  };
+
+  const pop = () => {
+    const newStack = [...stack];
+    const x = newStack.length - 1;
+    const y = Math.max(0, x - 1);
+
+    setBeforeValue(x);
+    setAfterValue(y);
+    setAnimationClass('show-animation');
+      setTemp('temp');
+    
+    setTimeout(() => {
+      if (newStack.length > 0) {
+        newStack.shift();
+        const amortizedCost = calculatePopAmortizedCost(x, y);
+        setAmortizedCost(amortizedCost);
+        updateTotalAmortizedCost(amortizedCost);
+      }
+      setStack(newStack);
+      setXValue(x);
+      setYValue(y);
+      setResultValue(amortizedCost);
+      setAnimationClass('');
+      setTemp('');
+    }, 1000);
+  };
+
+  const calculatePushAmortizedCost = (x, y) => {
+    return x === 0 ? 2 : 1 + y - x;
+  };
+
+  const calculatePopAmortizedCost = (x, y) => {
+    return x > 0 ? 1 + y - x : 0;
+  };
+
+  return (
+    <div className="container">
+      <div className="left-side">
+        <div className="buttons">
+          <button onClick={toggleInputField}>Push</button>
+          {inputVisible && (
+            <>
+              <input
+                type="text"
+                id="inputField"
+                placeholder="Enter"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <br />
+              <button onClick={push} id="confirmPush">
+                Confirm Push
+              </button>
+            </>
+          )}
+          <button onClick={pop}>Pop</button>
+        </div>
+        <div className="stack-container" id="stack-container">
+          {stack.map((item, index) => (
+            <div key={index} className="stack-item push-animation">
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="right-side">
+        <h2>Amortized Cost Calculation</h2>
+        <div className = {`enlarge ${temp}`} id="formula-values">
+          <p>
+            Φ(hi) = <span id="y-value">{yValue}</span>, Φ(hi-1) = <span id="x-value">{xValue}</span>,
+            Result = <span id="result-value">{resultValue}</span>
+          </p>
+        </div>
+        <p>
+          where, <br /> Φ(hi) Number of elements after operation
+        </p>
+        <p>Φ(hi-1) Number of elements before operation</p>
+        <img src={'./a'} alt="Description of your image" />
+        <p>
+          where, <br /> Φ(h) is potential function
+        </p>
+        <p> Ci is actual cost of the operation</p>
+        <div id="amortized-cost">Amortized Cost: {amortizedCost}</div>
+        <br />
+        <div id="total-amortized-cost">Total Amortized Cost: {totalAmortizedCost}</div>
+        <div className={`animation-container ${animationClass}`}>
+          <p>Number of elements before operation: {beforeValue}</p>
+          <p>Number of elements after operation: {afterValue}</p>
+          <p>Amortized Cost: {resultValue}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Visualize;
